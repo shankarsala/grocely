cdc71d68d9e954de5edf182e1c1accff4c838e92
shankarsala <shankarsala.dev@gmail.com>
2026-07-29 00:23:14 +0530

    Build Home page with reusable components

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import AppPromoBanner from "../components/Home/AppPromoBanner";
+import Features from "../components/Home/Features";
+import Hero from "../components/Home/Hero";
+import HomeCategories from "../components/Home/HomeCategories";
+import Newsletter from "../components/Home/Newsletter";
+import PopularProduct from "../components/Home/PopularProduct";
+
 const Home = () => {
-  return <div>Home</div>;
-};
+  return( 
+  <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <Hero />
+    <Features />
+    <HomeCategories/>
+    <PopularProduct/>
+    <AppPromoBanner />
+    <Newsletter />
+  </div>
+)};
 
 export default Home;
