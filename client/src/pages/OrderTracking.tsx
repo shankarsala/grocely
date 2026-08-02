908638062710d6b707083a431667cb4d3395f2ea
Shankar Sala <shankarsala9995@gmail.com>
2026-08-02 22:46:24 +0530

    Create My Orders page with responsive layout

--- client/src/pages/OrderTracking.tsx
+++ client/src/pages/OrderTracking.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+import type { Order } from "../types";
+import { dummyDashboardOrdersData } from "../assets/assets";
+import Loading from "../components/Loading";
+import { ArrowLeftIcon } from "lucide-react";
+
 
 
 const OrderTracking = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const [order, setOrder] = useState< Order | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [liveLocation, setLiveLocation] = useState<{lat: number; lng: number} | null>(null)
+
+
+  useEffect(()=> {
+    setOrder(dummyDashboardOrdersData.find((o)=>o._id === id) as any)
+    setLoading(false)
+  }, [id, navigate])
+
+  if(loading) return <Loading />
+  if(!order) null
+
   return (
-    <div>
-      Order Tracking
+    <div className="min-h-screen mb-20 bg-app-cream">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {/* Header */}
+        <button onClick={() => navigate("/orders")} className="flex items-center gap-2 text-sm text-app-text-light hover:text-app-green mb-6 transition-colors">
+          <ArrowLeftIcon className="size-4" /> Back to Orders
+        </button>
+
+        {/* order id, date, status */}
+      </div>
     </div>
   )
 }
