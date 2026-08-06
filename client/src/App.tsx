0af2c2c0c74131a5b93d4311bebf712565922f5a
Shankar Sala <shankarsala9995@gmail.com>
2026-08-06 17:42:27 +0530

    Create admin and delivery partner pages with routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,15 @@ import MyOrders from "./pages/MyOrders";
 import OrderTracking from "./pages/OrderTracking";
 import Adresses from "./pages/Addresses ";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminLayout from "./pages/admin/AdminLayout";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminProductForm from "./pages/admin/AdminProductForm";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners";
+import DeliveryLogin from "./pages/delivery/DeliveryLogin";
+import DeliveryLayout from "./pages/delivery/DeliveryLayout";
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard";
 
 const App = () => {
   return (
@@ -50,6 +59,24 @@ const App = () => {
             <Route path="addresses" element={<Adresses />} />
           </Route>
         </Route>
+        
+        {/* Admin Pages */}
+        <Route path="/admin" element={<AdminLayout/>}>
+           <Route index element={<AdminDashboard/>} />
+           <Route path="products" element={<AdminProducts/>} />
+           <Route path="products/new" element={<AdminProductForm/>} />
+           <Route path="products/:id/edit" element={<AdminProductForm/>} />
+           <Route path="orders" element={<AdminOrders/>} />
+           <Route path="delivery-partners" element={<AdminDeliveryPartners/>} />
+        </Route>
+
+
+        {/* Delivery Partner Pages */}
+        <Route path="/delivery/login" element={<DeliveryLogin/>} />
+        <Route path="/delivery" element={<DeliveryLayout/>}>
+           <Route index element={<DeliveryDashboard />}/>
+        </Route>
+
       </Routes>
     </>
   );
