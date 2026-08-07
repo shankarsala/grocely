194a96baa634a86c8d1cba2bd7aa14d54b1f503d
Shankar Sala <shankarsala9995@gmail.com>
2026-08-07 17:38:46 +0530

    Implement order management API routes

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
 
@@ -20,11 +21,12 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 app.use('/api/upload', uploadRouter)
+app.use('/api/orders', orderRouter)
 
 // Error handling
-app.use((error:any, req: Request, res: Response, next: NextFunction)=> {
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     console.error(error)
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
 })
 
 app.listen(port, () => {
